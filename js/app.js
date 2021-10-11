0fb9d88a00ce7d3f81d81058c1285dc22bb44fdd
ahmadhazm <1ahmadhazm@gmail.com>
2021-10-11 23:18:28 +0300

    create a new todo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
+const todoList = document.querySelector("#todosList")
+const createBar = document.querySelector("#createTodo")
+const itemsLeftEl = document.querySelector("#leftTodosNum")
+
+let itemsLeft = 1
+
+const removeIconUrl = "./images/remove.png"
+
+
+
+// create todo
+createBar.addEventListener("keydown",event=>{
+    if(event.key == "Enter" && createBar.value != ""){
+        let todoElCode = `
+            <input type="checkbox" name="checkbox" class="todoCheckbox">
+            <p class="todoText">${createBar.value}</p>
+            <img src="${removeIconUrl}" class="removeTodoBtn" alt="remove">`
+
+        const todo = document.createElement("li")
+        todo.classList.add("todo")
+        todo.innerHTML = todoElCode
+
+        todoList.insertBefore(todo,todoList.childNodes[0])
+        createBar.value = ""
+
+        itemsLeft++
+        itemsLeftEl.innerHTML = itemsLeft
+    }
+})
+
+
 function removeTodo(){}
 function clearCompleted(){}
 function filter(filter){}
