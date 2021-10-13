e586d5b61d9b7aec7d5c57958f0838f8de7cba3d
ahmadhazm <1ahmadhazm@gmail.com>
2021-10-13 15:44:14 +0300

    remove todo and completed todo

--- js/app.js
+++ js/app.js
@@ -1,34 +1,73 @@
-const todoList = document.querySelector("#todosList")
+const todoListEl = document.querySelector("#todosList")
 const createBar = document.querySelector("#createTodo")
 const itemsLeftEl = document.querySelector("#leftTodosNum")
 
-let itemsLeft = 1
+let itemsLeft = 0
 
-const removeIconUrl = "./images/remove.png"
+// resit left items
+function resitLeftItems(num){
+    itemsLeft += num
+    itemsLeftEl.innerHTML = itemsLeft
+}
 
+// create todo class
+class Todo{
+    constructor(value, container, classes, id){
+        this.value = value
+        this.container = container
+        this.classes = classes
+        this.removeIconUrl = "./images/remove.png"
+        this.id = id
 
+        this.todoElCode = `
+            <input type="checkbox" name="checkbox" class="todoCheckbox">
+            <p class="todoText">${this.value}</p>
+            <img id="_${this.id}" src="${this.removeIconUrl}" class="removeTodoBtn" alt="remove">`
+    }
+    createTodo(){
+        this.todoEl = document.createElement("li")
 
-// create todo
+        this.todoEl.classList.add(this.classes)
+        this.todoEl.innerHTML = this.todoElCode
+
+        this.container.insertBefore(this.todoEl, todoListEl.childNodes[0])
+        
+        document.querySelector(`#_${this.id}`).addEventListener("click",()=>{
+            this.removeTodo()
+        })
+    }
+    removeTodo(){
+        this.todoEl.remove()
+        resitLeftItems(-1)
+    }
+}
+
+// create todo when enter
+let TodoItemsArr = []
 createBar.addEventListener("keydown",event=>{
     if(event.key == "Enter" && createBar.value != ""){
-        let todoElCode = `
-            <input type="checkbox" name="checkbox" class="todoCheckbox">
-            <p class="todoText">${createBar.value}</p>
-            <img src="${removeIconUrl}" class="removeTodoBtn" alt="remove">`
+        // increse the left items
+        resitLeftItems(1)
 
-        const todo = document.createElement("li")
-        todo.classList.add("todo")
-        todo.innerHTML = todoElCode
+        // create a new todo and add it to the list(array) so we can filter them later
+        let todo = new Todo(createBar.value, todoListEl, "todo", itemsLeft)
+        TodoItemsArr.push(todo)
+        todo.createTodo()
 
-        todoList.insertBefore(todo,todoList.childNodes[0])
+        // clear the bar
         createBar.value = ""
-
-        itemsLeft++
-        itemsLeftEl.innerHTML = itemsLeft
     }
 })
 
+// clear all todo Items
+function clearCompleted(){
+    let checkBox = document.querySelectorAll(".todoCheckbox")
+    checkBox.forEach(e=>{
+        if(e.checked){
+            e.parentElement.remove()
+            resitLeftItems(-1)
+        }
+    })
+}
 
-function removeTodo(){}
-function clearCompleted(){}
 function filter(filter){}
